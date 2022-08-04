e98639df80c1195e05bf718bf4d8d903c42d7a1e
RutviMiskin <109785601+RutviMiskin@users.noreply.github.com>
2022-08-04 20:20:41 +0530

    Add files via upload

---
+++ ArrayEquality.java
@@ -0,0 +1,13 @@
+//  THis program is to test the equality of two arrays witten by Rutvi Miskin.
+
+
+import java.util.*;
+public class ArrayEquality
+{
+	public static void main(String args[])
+	{
+		int[] array1 = {1,2, 3, 4, 5};
+		int[] array2 = {1,2, 3, 4, 5};
+		System.out.println(Arrays.equals(array1, array2));  // equals() method is used to check if two arrays are equal.
+	}
+}
