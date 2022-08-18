1612e5bf877fec04d7ad7840a937001d5e8c812b
RutviMiskin <rm.miskin2001@gmail.com>
2022-08-18 20:47:25 +0530

    Activity

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+//  Write a Program to check the palindrome
+
+
+public class Palindrome{
+
+	public static void main(String[] args) {
+		
+		String s = "hello";
+		String rev = "";
+		for (int i = s.length()-1; i >=0 ; i--) 
+			rev=rev+s.charAt(i);
+		if(s.equals(rev))
+			System.out.println("String is palindrome");
+		else 
+			System.out.println("String is not palindrome");
+
+	}
+
+}
